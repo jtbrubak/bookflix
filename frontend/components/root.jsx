ac7806dac1eaf72575b15e992aede401ea408d80
Jeff Brubaker <jtbrubak1@gmail.com>
2018-05-12 00:10:12 -0400

    Add user/session function

---
+++ frontend/components/root.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+
+// react router
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+
+const Root = ({ store }) => {
+  return (
+    <p>YO</p>
+  );
+};
+
+export default Root;
